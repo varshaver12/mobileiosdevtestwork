5675ca26a2d199ec4e991d110e73a81723a5cf3e
varshaver12 <varshaver12@yandex.ru>
2024-03-13 12:47:47 +0300

    extensions Int, UILabel, UITableViewCell

---
+++ mobileiosdevtestwork/Extensions/UILabel+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  UILabel+Extensions.swift
+//  mobileiosdevtestwork
+//
+//  Created by Aleksey Khlestkin on 13.03.2024.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String? = "",
+                     textColor: UIColor? = UIColor.darkText,
+                     font: UIFont? = UIFont(name: "SFProDisplay-Regular", size: 16),
+                     isHidden: Bool? = false,
+                     textAlignment: NSTextAlignment? = .natural
+    ) {
+        self.init()
+        self.text = text
+        self.textColor = textColor
+        self.font = font
+        self.isHidden = isHidden ?? false
+        self.textAlignment = textAlignment ?? .left
+        self.adjustsFontSizeToFitWidth = true
+        self.minimumScaleFactor = 0.5
+        self.numberOfLines = 1
+        self.lineBreakMode = .byWordWrapping
+    }
+    
+}

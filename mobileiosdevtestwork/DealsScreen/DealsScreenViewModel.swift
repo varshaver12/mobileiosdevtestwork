c5e3c57d8ddf39187fc6450272705a45d83d126e
varshaver12 <varshaver12@yandex.ru>
2024-03-16 19:44:49 +0300

    Made a common view model

--- mobileiosdevtestwork/DealsScreen/DealsScreenViewModel.swift
+++ mobileiosdevtestwork/DealsScreen/DealsScreenViewModel.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 protocol IDealsScreenViewModel {
-    func openSortingSettingsScreen(initialViewController: DealsScreenView, currentSort: (DealsSorting, SortOrder))
+    func openSortingSettingsScreen(initialViewController: DealsScreenView)
+    func getNewDeal()
+    var content: [Deal] { get set}
+    var currentSort: (DealsSorting, SortOrder) { get set }
+    var lockAppend: NSLock { get }
 }
 
 final class DealsScreenViewModel: IDealsScreenViewModel {
     
-    func openSortingSettingsScreen(initialViewController: DealsScreenView, currentSort: (DealsSorting, SortOrder)) {
+    private lazy var server = Server()
+    let lockAppend = NSLock()
+    var content: [Deal] = []
+    
+    var currentSort: (DealsSorting, SortOrder) = (.dealModificationDate, .descending) {
+        didSet {
+            NotificationCenter.default.post(name: Notification.Name("SortUpdated"), object: nil)
+        }
+    }
+    
+    func getNewDeal() {
+        server.subscribeToDeals { [weak self] deals in
+            DispatchQueue(label: "getNewDeal").async {
+                guard let self = self else { return }
+                self.lockAppend.lock()
+                self.content.append(contentsOf: deals)
+                self.lockAppend.unlock()
+                NotificationCenter.default.post(name: Notification.Name("DataUpdated"), object: nil)
+            }
+        }
+    }
+    
+    func openSortingSettingsScreen(initialViewController: DealsScreenView) {
         let bottomSheetViewModel = BottomSheetViewModel(delegate: initialViewController, currentSort: currentSort)
         let bottomSheet = BottomSheetTableView(viewModel: bottomSheetViewModel, currentSort: currentSort)
         if let sheet = bottomSheet.sheetPresentationController {
@@ -31,3 +57,4 @@ private extension DealsScreenViewModel {
         static let sheetCornerRadius: CGFloat = 20
     }
 }
+
